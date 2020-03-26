0b38e22b6ad727300ef627533d2e0536b97a86ba
MihaiZhaoDev <mihai.zhao@mail.bcu.ac.uk>
2020-03-26 13:08:32 +0200

    Add to the error handler a http error

--- tools/handler.js
+++ tools/handler.js
@@ -1,7 +1,14 @@
-exports.error = function(err, req, res) {
-    console.log('Error occurred at ' + req.originalUrl);
+const createError = require('http-errors');
+
+exports.error = function (err, req, res) {
+    // Log the error
+    console.error('Error occurred at ' + req.originalUrl);
     console.error(err);
 
-    if(req.xhr) return res.json({success: false, message: 'Internal error.'});
-    else return res.send('There are some internal errors.');
+    // Create a http error
+    const httpError = createError(404, 'There are some internal errors. Please try again.');
+
+    // Send back the response
+    if (req.xhr) return res.json({success: false, status: httpError.statusCode, message: httpError.message});
+    else return res.send(httpError.message);
 };
